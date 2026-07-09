04e03fb2c3f17f82026695e1a69bc7b05ca826a2
mab <maxmaxsolovev@mail.ru>
2026-07-09 11:36:24 +0300

    Создание репозитория

---
+++ database.py
@@ -0,0 +1,9 @@
+from json_db_lite import JSONDatabase
+
+class Heroes:
+    pass
+
+class Hero:
+    heroes_db = Heroes()
+    def __init__(self, name: str, race: str, character_class: str, lvl: int, skills: list, destribition: str):
+        pass
